9a7c1e6eaba93333ce745a4a584c2ade068b7fe8
miguelsndc <mglsndc@gmail.com>
2021-07-31 00:22:53 -0300

    feat: setup theme & routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+
+import Home from './pages/Home'
+import Login from './pages/Login'
+
+import GlobalStyles from './styles/globals'
+import theme from './styles/theme'
+
+import './services/firebase'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <main>
+        <Router>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/login' component={Login} />
+          </Switch>
+        </Router>
+      </main>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
