36fc38dc94b13bab786e78c464daaeef4b6585ac
miguelsndc <mglsndc@gmail.com>
2021-07-31 00:14:17 -0300

    feat: add theme

---
+++ src/styles/styled.d.ts
@@ -0,0 +1,24 @@
+import 'styled-components'
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    gray50: string
+    gray100: string
+    gray200: string
+    gray300: string
+    gray400: string
+    gray500: string
+    gray600: string
+    gray700: string
+    gray800: string
+    gray900: string
+    fuchsia300: string
+    fuchsia400: string
+    fuchsia500: string
+    fuchsia600: string
+    fuchsia700: string
+    fuchsia800: string
+    fuchsia900: string
+    warning: string
+  }
+}
---
+++ src/styles/theme.js
@@ -0,0 +1,22 @@
+const theme = {
+  gray50: '#FAFAFA',
+  gray100: '#F5F5F5',
+  gray200: '#E5E5E5 ',
+  gray300: '#D4D4D4',
+  gray400: '#A3A3A3',
+  gray500: '#737373',
+  gray600: '#525252',
+  gray700: '#404040',
+  gray800: '#262626',
+  gray900: '#171717',
+  fuchsia300: '#F0ABFC',
+  fuchsia400: '#E879F9',
+  fuchsia500: '#D946EF',
+  fuchsia600: '#C026D3',
+  fuchsia700: '#A21CAF',
+  fuchsia800: '#86198F',
+  fuchsia900: '#701A75',
+  warning: '#b32c27',
+}
+
+export default theme
