688feb5559cecb8dd7e7b8623b442ebafce668c8
miguelsndc <mglsndc@gmail.com>
2021-08-03 15:59:32 -0300

    feat(Channel): listen to realtime updates

--- src/components/Channel/index.js
+++ src/components/Channel/index.js
@@ -1,39 +1,76 @@
 import { useEffect } from 'react'
+import { useForm } from 'react-hook-form'
 import { useTheme } from 'styled-components'
-
+import { useDispatch, useSelector } from 'react-redux'
 import { AiOutlineSend } from 'react-icons/ai'
 
 import { Messages, Form, Wrapper } from './styles'
-import { useDispatch, useSelector } from 'react-redux'
-import { createMessageAsync, LOAD_MESSAGES } from '../../store/ducks/channel'
-import { loadMessages } from '../../api/helpers'
+import { LOAD_MESSAGES } from '../../store/ducks/channel'
+import { createNewMessage } from '../../api/helpers'
+
+import Message from '../Message'
+
+import { db } from '../../services/firebase'
 
 const Channel = () => {
   const theme = useTheme()
   const dispatch = useDispatch()
   const messages = useSelector(state => state.channel.messages)
+  const user = useSelector(state => state.auth.user)
+  const { register, handleSubmit, reset } = useForm()
 
-  function handleCreateMessage(e) {
-    e.preventDefault()
+  function handleCreateMessage(data) {
+    const { message } = data
 
-    dispatch(createMessageAsync({ message: 'testando' }))
+    const newMessage = {
+      content: message,
+      author: {
+        photoURL: user.photoURL,
+        name: user.displayName,
+        id: user.uid,
+      },
+      createdAt: new Date(),
+    }
+
+    createNewMessage(newMessage)
+
+    reset({
+      message: '',
+    })
   }
 
   useEffect(() => {
-    loadMessages().then(messages => {
-      dispatch({ type: LOAD_MESSAGES, payload: { messages } })
-    })
+    const unsubscribe = db
+      .collection('channel')
+      .orderBy('createdAt')
+      .onSnapshot(snapshot => {
+        const messages = snapshot.docs.map(doc => ({
+          id: doc.id,
+          ...doc.data(),
+        }))
+        dispatch({ type: LOAD_MESSAGES, payload: { messages } })
+      })
+
+    return () => unsubscribe()
   }, [dispatch])
 
   return (
     <Wrapper>
       <Messages>
-        {messages.map(message => {
-          return <span key={message.id}>{message.message}</span>
-        })}
+        {messages &&
+          messages.map(message => {
+            return (
+              <Message
+                key={message.id}
+                author={message.author}
+                content={message.content}
+                createdAt={message.createdAt}
+              />
+            )
+          })}
       </Messages>
-      <Form onSubmit={handleCreateMessage}>
-        <input type='text' />
+      <Form onSubmit={handleSubmit(handleCreateMessage)}>
+        <input type='text' {...register('message', { required: true })} />
         <button type='submit'>
           <AiOutlineSend size='1.7rem' color={theme.gray300} />
         </button>
