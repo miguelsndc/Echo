6919f3342f84433ffdaabc7e3879774e9e8dcb9b
miguelsndc <mglsndc@gmail.com>
2021-08-03 15:57:49 -0300

    refactor: remove unused code

--- src/store/ducks/channel.js
+++ src/store/ducks/channel.js
@@ -1,5 +1,3 @@
-import { createNewMessage } from '../../api/helpers'
-
 // Actions
 
 export const ADD_NEW_MESSAGE = 'echo/channel/ADD_NEW_MESSAGE'
@@ -14,10 +12,6 @@ const INITIAL_STATE = {
 
 export default function reducer(state = INITIAL_STATE, action) {
   switch (action.type) {
-    case ADD_NEW_MESSAGE:
-      return {
-        messages: [...state.messages, action.payload.message],
-      }
     case REMOVE_MESSAGE:
       return {
         messages: state.messages.filter(
@@ -35,23 +29,7 @@ export default function reducer(state = INITIAL_STATE, action) {
 
 // Action Creators
 
-export const addMessage = message => ({
-  type: ADD_NEW_MESSAGE,
-  payload: {
-    message,
-  },
-})
-
 export const removeMessage = id => ({
   type: REMOVE_MESSAGE,
   payload: { id },
 })
-
-// Side effects
-
-export const createMessageAsync = message => {
-  return async dispatch => {
-    const newMessage = await createNewMessage(message)
-    dispatch(addMessage(newMessage))
-  }
-}
